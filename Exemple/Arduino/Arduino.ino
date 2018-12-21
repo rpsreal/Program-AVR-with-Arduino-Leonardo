11a18e0a12e3289e26893a14f0fa99bc74cad2f5
Rui Silva <ruisilva.real@sapo.pt>
2018-12-21 20:21:21 +0000

    Add files via upload

---
+++ Exemple/Arduino/Arduino.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // Connect pin 9 to a LED and a resistor (1kohm)
+  pinMode(9, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(9, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(9, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
